0774cdd30a560219244ac210efa2e52e68ff9244
AnnefranceCosta <annefrance.costa@gmail.com>
2023-10-18 20:11:01 -0300

    Tipos basicos

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -6,7 +6,36 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World!");
+            bool completo = false;
+            char condicao = 'v';
+            char letra = '\u0041';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.8;
+            string nome = "Annefrance Costa";
+            
+            // tipo object aceita qualquer tipo 
+
+            
+
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(completo);
+            Console.WriteLine(condicao);
+            Console.WriteLine(letra);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+
+
+            int n7 = int.MinValue;
+            Console.WriteLine(n7);
         }
     }
 }
