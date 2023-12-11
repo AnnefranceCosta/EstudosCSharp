3feca287a94db383db572ca1dae9004dd2cb3ad3
AnnefranceCosta <annefrance.costa@gmail.com>
2023-12-11 20:23:56 -0300

    Saida de dados

---
+++ SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SaidaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            int idade = 26;
+            double saldo = 506.05;
+            string nome = "Annefrance Costa";
+
+            Console.Write("Bom dia");
+            Console.WriteLine("Boa tarde");
+            Console.WriteLine("Boa noite");
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
