51b77fe5fcd202a8cb28c84dbb0cd8fd652d7526
AnnefranceCosta <annefrance.costa@gmail.com>
2024-01-08 18:30:25 -0300

    Implementação do calculo da área do triangulo sem utilização da programação orientada a objetos

--- ProblemaSemOO/Program.cs
+++ ProblemaSemOO/Program.cs
@@ -1,6 +1,7 @@
 ﻿//Fazer um programa para ler as medidas dos lados de dois triangulos X e Y (suponha medidas v).
 //Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triãngulos
 // possui a maior área.
+using System.Globalization;
 
 
 namespace ProblemaSemOO
@@ -9,7 +10,37 @@ namespace ProblemaSemOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World"); ;
+            double xA, xB, xC, yA, yB, yC;
+
+
+            Console.WriteLine("Entre com as medidas do triângulos X: ");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulos Y: ");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área é do X.");
+            }
+            else
+            {
+                Console.WriteLine("Maior área é do Y.");
+            }
         }
     }
 }
