d355dd9ed4c378712aee6414d3c93d9076f1d9ef
AnnefranceCosta <annefrance.costa@gmail.com>
2024-01-08 18:05:27 -0300

    commit teste

--- ProblemaSemOO/Program.cs
+++ ProblemaSemOO/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ProblemaSemOO
+﻿//Fazer um programa para ler as medidas dos lados de dois triangulos X e Y (suponha medidas v).
+//Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triãngulos
+// possui a maior área.
+
+
+namespace ProblemaSemOO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World"); ;
         }
     }
 }
