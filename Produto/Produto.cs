819eb22d928b8540b13bf13dff2031004e5614d2
AnnefranceCosta <annefrance.costa@gmail.com>
2024-01-12 04:50:01 -0300

    Programa de cadastro de produto em estoque pegando: nome, preco e quantidade.

---
+++ Produto/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Produto
+{
+    internal class Produto
+    {
+        public String Name;
+        public double Price;
+        public int Numbers;
+
+    }
+}
---
+++ Produto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+
+namespace Produto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+
+            Console.Write("Nome: ");
+            p.Name = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Numbers = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
